7d2dba095c69348615ce0f4fa3745d175ba35b8c
Ayza <7302512@gmail.com>
2023-01-10 23:13:55 -0500

    init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let num=Math.abs(n).toString();
+    let a="";
+    for (let i = num.length; i > 0; i--) {
+        a = a + num[i-1];
+    }
+    return a;
 }
